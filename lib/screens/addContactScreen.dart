9d790a690263cad7993999f44385e5661c4c2e15
2021143818 <a2021143818@isec.pt>
2024-12-20 15:52:32 +0000

    AddContactScreen(Add photo)

--- lib/screens/addContactScreen.dart
+++ lib/screens/addContactScreen.dart
@@ -1,7 +1,10 @@
 
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/models/contact.dart';
+import 'package:image_picker/image_picker.dart';
 
 class addContactScreen extends StatefulWidget{
 
@@ -16,6 +19,8 @@ class _addContactScreen extends State<addContactScreen>{
   late TextEditingController newName;
   late TextEditingController newEmail;
   late TextEditingController newPhone;
+  File? _selectedImage;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState(){
@@ -23,6 +28,15 @@ class _addContactScreen extends State<addContactScreen>{
     newName = TextEditingController();
     newEmail = TextEditingController();
     newPhone = TextEditingController();
+  }
+
+  Future<void> _pickImage(ImageSource source) async {
+    final XFile? pickedFile = await _picker.pickImage(source: source);
+    if (pickedFile != null){
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+      });
+    }
   }
 
   @override
@@ -37,7 +51,8 @@ class _addContactScreen extends State<addContactScreen>{
               final newContact = Contact(
                   name: newName.text,
                   email: newEmail.text,
-                  phone: newPhone.text
+                  phone: newPhone.text,
+                  imagePath: _selectedImage?.path
               );
               Navigator.pop(context, newContact);
             },
@@ -48,6 +63,31 @@ class _addContactScreen extends State<addContactScreen>{
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            if(_selectedImage != null)
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: FileImage(_selectedImage!),
+              )
+            else
+              const CircleAvatar(
+                radius: 60,
+                child: Icon(Icons.person, size: 60),
+              ),
+            const SizedBox(height: 16,),
+            ElevatedButton.icon(
+              onPressed: () {
+                _pickImage(ImageSource.gallery);
+              },
+              icon: const Icon(Icons.photo),
+              label: const Text("Choose from gallery"),
+            ),
+            ElevatedButton.icon(
+                onPressed: () {
+                  _pickImage(ImageSource.camera);
+                },
+                icon: const Icon(Icons.camera),
+                label: const Text("Take a photo"),
+            ),
             TextField(
               controller: newName,
               decoration: const InputDecoration(labelText: 'Name'),
