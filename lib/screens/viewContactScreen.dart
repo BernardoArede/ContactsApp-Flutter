0b80e7278c33e4f3af7c36a7309c7b210858bc8f
RicardoDR14 <ricdr2004@gmail.com>
2024-12-23 22:54:19 +0000

    mapa atualiza com a posição da localização atual

--- lib/screens/viewContactScreen.dart
+++ lib/screens/viewContactScreen.dart
@@ -1,23 +1,40 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/screens/editContactScreen.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../models/contact.dart';
 
 class viewContactScreen extends StatelessWidget {
   final Contact contact;
-  const viewContactScreen({super.key, required this.contact});
+  LatLng initialCameraPosition = const LatLng(37.7749, -122.4194);
+  late GoogleMapController mapController;
 
-  static const initialCameraPosition = CameraPosition(
-    target: LatLng(37.7749, -122.4194),
-    zoom: 12,
-  );
+  viewContactScreen({super.key, required this.contact});
 
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permitions.');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Contact Details"),
@@ -31,10 +48,11 @@ class viewContactScreen extends StatelessWidget {
             CircleAvatar(
               radius: 60,
               backgroundImage: contact.imagePath != null
-              ? FileImage(File(contact.imagePath!)) : null,
-              child:
-                  contact.imagePath == null
-                      ? Icon(Icons.person, size: 60, color: Colors.white) : null,
+                  ? FileImage(File(contact.imagePath!))
+                  : null,
+              child: contact.imagePath == null
+                  ? Icon(Icons.person, size: 60, color: Colors.white)
+                  : null,
               backgroundColor: Theme.of(context).primaryColor,
             ),
             const SizedBox(height: 16),
@@ -55,42 +73,77 @@ class viewContactScreen extends StatelessWidget {
             const SizedBox(height: 16),
             _buildDetailRow(Icons.email, 'Email', contact.email),
             const SizedBox(height: 16),
-            if(contact.birthdate != null)
+            if (contact.birthdate != null)
               _buildDetailRow(Icons.cake, 'Birthdate', contact.birthdate!),
-              const SizedBox(height: 8),
-              SizedBox(
+            const SizedBox(height: 8),
+            SizedBox(
               height: 200, // Defina a altura desejada para o mapa.
-              child: GoogleMap(
-              initialCameraPosition: initialCameraPosition,
-              mapType: MapType.normal,
-              myLocationEnabled: true,
-              myLocationButtonEnabled: true,
-              zoomControlsEnabled: false,
-              onMapCreated: (GoogleMapController controller) {
-              // Salve ou use o controlador, se necessário.
-              },
+              child: Stack(
+                children: [
+                  GoogleMap(
+                    initialCameraPosition: CameraPosition(
+                      target: initialCameraPosition,
+                      zoom: 15,
+                    ),
+                    mapType: MapType.normal,
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    zoomControlsEnabled: false,
+                    onMapCreated: (GoogleMapController controller) async {
+                      Position position = await getCurrentLocation();
+                      mapController = controller;
+                      mapController.animateCamera(
+                        CameraUpdate.newCameraPosition(
+                          CameraPosition(
+                            target:
+                                LatLng(position.latitude, position.longitude),
+                            zoom: 15,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  Positioned(
+                    bottom: 16, // Distância da parte inferior
+                    right: 16, // Distância da lateral direita
+                    child: FloatingActionButton(
+                      onPressed: () async {
+                        Position position = await getCurrentLocation();
+                        mapController.animateCamera(
+                          CameraUpdate.newCameraPosition(
+                            CameraPosition(
+                              target:
+                                  LatLng(position.latitude, position.longitude),
+                              zoom: 15,
+                            ),
+                          ),
+                        );
+                      },
+                      child: const Icon(Icons.my_location),
+                    ),
+                  ),
+                ],
               ),
-              ),
+            ),
             const Spacer(),
             Center(
-              child: ElevatedButton.icon(
-                  onPressed: () async{
-                    final updateContact =  await Navigator.push<Contact>(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => EditContactScreen(contact: contact),
-                      ),
-                  );
-                    if(updateContact != null){
-                      Navigator.pop(context, updateContact);
-                    }else{
-                      Navigator.pop(context);
-                    }
-                  },
-                  icon: const Icon(Icons.edit),
-                  label: const Text("Edit contact"),
-              )
-            )
+                child: ElevatedButton.icon(
+              onPressed: () async {
+                final updateContact = await Navigator.push<Contact>(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditContactScreen(contact: contact),
+                  ),
+                );
+                if (updateContact != null) {
+                  Navigator.pop(context, updateContact);
+                } else {
+                  Navigator.pop(context);
+                }
+              },
+              icon: const Icon(Icons.edit),
+              label: const Text("Edit contact"),
+            ))
           ],
         ),
       ),
